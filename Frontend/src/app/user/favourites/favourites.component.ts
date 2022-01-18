431515f8fcaf53c0c31ce61295a29eac559d8db8
Ronak <random07use@gmail.com>
2022-01-18 22:48:24 +0530

    seach added in header

--- Frontend/src/app/user/favourites/favourites.component.ts
+++ Frontend/src/app/user/favourites/favourites.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favourites.component.css']
 })
 export class FavouritesComponent implements OnInit {
-  isFavouriteChef = true;
+  isFavouriteChef = false;
   constructor() { }
 
   ngOnInit(): void {
