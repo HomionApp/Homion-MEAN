101249d6d4e15c1f2b0fe4597c3778ce3a78aeaf
hardik221 <hardikamareliya221@gmail.com>
2022-02-09 23:27:15 +0530

    form validation-Product

--- Frontend/src/app/chef/products/add-product/add-product.component.ts
+++ Frontend/src/app/chef/products/add-product/add-product.component.ts
@@ -8,27 +8,27 @@ import { ChefService } from '../../chef.service';
   styleUrls: ['./add-product.component.css'],
 })
 export class AddProductComponent implements OnInit {
-  productForm!: FormGroup;
   categories: { _id: string; name: string }[] = [];
   subCategories: { _id: string; name: string }[] = [];
+  imgPath = "";
+
+  form = new FormGroup({
+    productName: new FormControl(null, Validators.required),
+    category: new FormControl(null, Validators.required),
+    subCategory: new FormControl(null, Validators.required),
+    price: new FormControl(null, [Validators.required, Validators.min(1)]),
+    unitValue: new FormControl(null, [Validators.required, Validators.min(1)]),
+    unit: new FormControl('gm', Validators.required),
+    prepTime: new FormControl(null, Validators.required),
+    openForHome: new FormControl('no', Validators.required),
+    jainAvailable: new FormControl('no', Validators.required),
+    isSpeciality: new FormControl('no', Validators.required),
+    productImage: new FormControl(null, Validators.required),
+  });
 
   constructor(private chefService: ChefService) {}
 
   ngOnInit(): void {
-    this.productForm = new FormGroup({
-      productName: new FormControl(null, Validators.required),
-      category: new FormControl(null, Validators.required),
-      subCategory: new FormControl(null, Validators.required),
-      price: new FormControl(null, Validators.required),
-      unitValue: new FormControl(null, Validators.required),
-      unit: new FormControl('gm', Validators.required),
-      prepTime: new FormControl(null, Validators.required),
-      openForHome: new FormControl('no', Validators.required),
-      jainAvailable: new FormControl('no', Validators.required),
-      isSpeciality: new FormControl('no', Validators.required),
-      productImage: new FormControl(null, Validators.required),
-    });
-
     this.getCategories();
     this.getSubCategories();
   }
@@ -37,7 +37,7 @@ export class AddProductComponent implements OnInit {
     this.chefService.getCategories().subscribe((res) => {
       if (res.status == 200) {
         this.categories = res.data;
-        this.productForm.patchValue({
+        this.form.patchValue({
           category: this.categories[0]?._id,
         });
       }
@@ -48,14 +48,34 @@ export class AddProductComponent implements OnInit {
     this.chefService.getSubCategories().subscribe((res) => {
       if (res.status == 200) {
         this.subCategories = res.data;
-        this.productForm.patchValue({
+        this.form.patchValue({
           subCategory: this.subCategories[0]?._id,
         });
       }
     });
   }
 
+  fileChangeEvent(event: any) {
+    const files = event.target.files;
+    let file;
+    if(files){
+      file = files[0];
+      
+      const reader = new FileReader();
+      reader.readAsDataURL(file)
+      reader.onload = (_event) => {
+        this.imgPath = reader.result as string; 
+      }
+      console.log(this.imgPath)
+    }
+    // this.filesToUpload = event.srcElement.files;
+    // console.log(this.filesToUpload);
+    // this.fileInput = event.srcElement;
+ }
+
   addProduct() {
-    console.log(this.productForm);
+    console.log(this.form);
   }
+
+  
 }
